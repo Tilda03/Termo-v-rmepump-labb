f17a70e033924fbb226aae659a50dfa1f345fc01
Tilda03 <tilda6573@gmail.com>
2026-03-02 09:41:24 +0100

    data

--- Värmepump.py
+++ Värmepump.py
@@ -3,7 +3,18 @@ import numpy as np
 
 data = np.loadtxt("Data.txt", delimiter="\t", skiprows=1)
 
-x = data[:, 0]   # first column
-y = data[:, 1]   # second column
+minutes = data[:, 0]   # first column
+T_w = data[:, 1]   # second column
+T_k = data[:, 2]
+P_effect = data[:, 3]
+T_1 = data[:, 4]
+T_2 = data[:, 5]
+T_3 = data[:, 6]
+T_4 = data[:, 7]
+P_1 = data[:, 8]
+P_2 = data[:, 9]
+
 
 print(data)
+
+print(T_3)
