dda1b9a36ef636619aeb99219edc3a4ffd9ffb7d
Tilda03 <tilda6573@gmail.com>
2026-03-02 09:44:27 +0100

    data

--- Värmepump.py
+++ Värmepump.py
@@ -1,10 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 data = np.loadtxt("Data.txt", delimiter="\t", skiprows=1)
 
-minutes = data[:, 0]   # first column
-T_w = data[:, 1]   # second column
+minutes = data[:, 0] 
+T_w = data[:, 1]  
 T_k = data[:, 2]
 P_effect = data[:, 3]
 T_1 = data[:, 4]
@@ -18,3 +19,10 @@ P_2 = data[:, 9]
 print(data)
 
 print(T_3)
+
+plt.scatter(minutes, T_1, label='T1')
+plt.scatter(minutes, T_2, label='T2')
+plt.scatter(minutes, T_3, label='T3')
+plt.scatter(minutes, T_4, label='T4')
+plt.legend()
+plt.show()
